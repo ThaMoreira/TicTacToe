92307de4bb8e53dcc5e60c2a2909756d22d7bebc
Maya Carvalho <mayaracarvalhomk@gmail.com>
2022-10-06 17:24:26 -0300

    Minha parte

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -11,5 +11,28 @@ public class Board {
                 System.out.println("  ---------------");
         }
     }
+    public static int verifyBoard(char [][] board) {
+        int count = 0;
+
+        for (int i = 0; i < 3; i++)
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][0] == board[i][2])
+                return 1;
+        for (int i = 0; i < 3; i++)
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[0][i] == board[2][i])
+                return 1;
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2])
+            return 1;
+        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[0][2] == board[2][0])
+            return 1;
+        else {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (board[i][j] != ' ')
+                        count++;
+            if (count == 9)
+                return 2;
+        }
+        return 0;
+    }
 
 }
--- TicTacToe/src/Game.java
+++ TicTacToe/src/Game.java
@@ -1,10 +1,73 @@
+
 import java.util.Scanner;
 
 public class Game {
-
     public static void playGame(char [][] board, Players player1, Players player2) {
         Board.showBoard(board);
+        int over = 0;
+        while (over == 0) {
+            over = getShot(board, player1, over);
+            if (over == 0)
+                over = getShot(board, player2, over);
+        }
+        System.out.printf("Scoreboard: %s %d x %s %d",
+                player1.name, player1.points, player2.name, player2.points);
+        newGame(board, player1, player2);
 
+    }
+    public static int getShot(char [][] board, Players player, int over) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(player.name + " it's your turn to play (" + player.simb + "): ");
+        String shot = input.nextLine();
+        if (shot.length() != 2) {
+            System.err.println("Enter two numbers!! Try again...\n");
+            getShot(board, player, over);
+        } else {
+            int row = shot.charAt(0) - '0';
+            int column = shot.charAt(1) - '0';
+            if (row < 0 || row > 2 || column < 0 || column > 2) {
+                System.err.println("Invalid move!! Try again...\n");
+                getShot(board, player, over);
+            } else if (board[row][column] != ' ') {
+                System.err.println("The square is already filled!! Try again...\n");
+                getShot(board, player, over);
+            } else {
+                board[row][column] = player.simb;
+                Board.showBoard(board);
+                over = winGame(board, player, over);
+            }
+        }
+        return over;
+    }
+
+    public static int winGame(char [][] board, Players player, int over){
+        int win;
+
+        win = Board.verifyBoard(board);
+        switch (win)
+        {
+            case 1:
+                System.out.printf("\n%s win!!\nEnd game.\n", player.name);
+                player.points++;
+                over = 1;
+                break;
+            case 2:   System.out.println("\nEnd game. Tied game.");
+                over = 1;
+                break;
+            default: break;
+        }
+        return over;
+    }
+
+    public static void newGame(char [][] board, Players player1, Players player2) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n\nPlay again? (y/n)");
+        String again = input.nextLine();
+        if (again.equalsIgnoreCase("y")) {
+            Board.initBoard(board);
+            FinalProject.Game.playGame(board, player1, player2);
+        }
     }
 
 }
